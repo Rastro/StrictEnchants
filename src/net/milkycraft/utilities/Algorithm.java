199bf8fb7ab01a008facf67259de62c861b9fc06
Nick Porillo <milkywayz@mail.com>
2012-08-04 00:35:10 -0400

    Added a lot of core code #GettingCloser :)

--- src/net/milkycraft/utilities/Algorithm.java
+++ src/net/milkycraft/utilities/Algorithm.java
@@ -1,13 +1,15 @@
 package net.milkycraft.utilities;
 
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
 import net.milkycraft.StricterEnchant;
 import net.milkycraft.configuration.Settings;
+import net.milkycraft.events.EnchantModifyEvent;
 import net.milkycraft.listeners.EnchantListener;
+import net.milkycraft.utilities.Utility;
 
-import org.bukkit.ChatColor;
+import org.bukkit.Bukkit;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
@@ -24,6 +26,7 @@ public class Algorithm extends StricterEnchant {
 
 	/** The alg. */
 	public static Algorithm alg;
+	public Map<Enchantment, Integer> res;
 
 	/**
 	 * Calculate the enchantments then call Ulility.enchant()
@@ -44,13 +47,17 @@ public class Algorithm extends StricterEnchant {
 					+ " levels");
 		}
 		if (isHigh(en.getName())) {
-			b(e, i, isWild(Settings.wildhigh), Group.HIGH);
+			b(e, i, isWild(Settings.wildhigh), l, Group.HIGH);
+			Utility.debug("Wild: "  + isWild(Settings.wildhigh));
 		} else if (isMedium(en.getName())) {
-			b(e, i, isWild(Settings.wildmed), Group.MEDIUM);
+			b(e, i, isWild(Settings.wildmed), l, Group.MEDIUM);
+			Utility.debug("Wild: "  + isWild(Settings.wildmed));
 		} else if (isLow(en.getName())) {
-			b(e, i, isWild(Settings.wildlow), Group.LOW);
+			b(e, i, isWild(Settings.wildlow), l, Group.LOW);
+			Utility.debug("Wild: "  + isWild(Settings.wildlow));
 		} else if (isDefault(en.getName())) {
-			b(e, i, isWild(Settings.wilddef), Group.DEFAULT);
+			b(e, i, isWild(Settings.wilddef), l, Group.DEFAULT);
+			Utility.debug("Wild: "  + isWild(Settings.wilddef));
 		}
 	}
 
@@ -73,27 +80,37 @@ public class Algorithm extends StricterEnchant {
 	 *            the group
 	 */
 	private void b(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+			int l, Group group) {
 		if (getMultiplier(group) <= 10) {
-			c(ens, item, wild, group);
+			c(ens, item, l, wild, group);
+			Utility.debug("Multiplier <= 10");
 		} else if (getMultiplier(group) <= 20) {
-			d(ens, item, wild, group);
+			d(ens, item, l, wild, group);
+			Utility.debug("Multiplier <= 20");
 		} else if (getMultiplier(group) <= 30) {
-			e(ens, item, wild, group);
+			e(ens, item, l, wild, group);
+			Utility.debug("Multiplier <= 30");
 		} else if (getMultiplier(group) <= 40) {
-			f(ens, item, wild, group);
+			f(ens, item, l, wild, group);
+			Utility.debug("Multiplier <= 40");
 		} else if (getMultiplier(group) <= 50) {
-			g(ens, item, wild, group);
+			g(ens, item, l, wild, group);
+			Utility.debug("Multiplier <= 50");
 		} else if (getMultiplier(group) <= 60) {
-			h(ens, item, wild, group);
+			h(ens, item, l, wild, group);
+			Utility.debug("Multiplier <= 60");
 		} else if (getMultiplier(group) <= 70) {
-			i(ens, item, wild, group);
+			i(ens, item, l, wild, group);
+			Utility.debug("Multiplier <= 70");
 		} else if (getMultiplier(group) <= 80) {
-			j(ens, item, wild, group);
+			j(ens, item, l, wild, group);
+			Utility.debug("Multiplier <= 80");
 		} else if (getMultiplier(group) <= 90) {
-			k(ens, item, wild, group);
+			k(ens, item, l, wild, group);
+			Utility.debug("Multiplier <= 00");
 		} else if (getMultiplier(group) < 100) {
-			l(ens, item, wild, group);
+			l(ens, item, l, wild, group);
+			Utility.debug("Multiplier < 100");
 		}
 	}
 
@@ -109,8 +126,8 @@ public class Algorithm extends StricterEnchant {
 	 * @param group
 	 *            the group
 	 */
-	private void c(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void c(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
@@ -118,7 +135,14 @@ public class Algorithm extends StricterEnchant {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
 		}
-		// Throw EnchantModifyEvent with the correct parameters
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
+		}
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		this.res.clear();
 	}
 
 	/**
@@ -128,13 +152,15 @@ public class Algorithm extends StricterEnchant {
 	 *            the ens
 	 * @param item
 	 *            the item
+	 * @param l
 	 * @param wild
 	 *            the wild
 	 * @param group
 	 *            the group
 	 */
-	private void d(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void d(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
+
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
@@ -142,7 +168,14 @@ public class Algorithm extends StricterEnchant {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
 		}
-		// Throw EnchantModifyEvent with the correct parameters
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
+		}
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		this.res.clear();
 	}
 
 	/**
@@ -152,13 +185,14 @@ public class Algorithm extends StricterEnchant {
 	 *            the ens
 	 * @param item
 	 *            the item
+	 * @param l
 	 * @param wild
 	 *            the wild
 	 * @param group
 	 *            the group
 	 */
-	private void e(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void e(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
@@ -166,7 +200,14 @@ public class Algorithm extends StricterEnchant {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
 		}
-		// Throw EnchantModifyEvent with the correct parameters
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
+		}
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		this.res.clear();
 	}
 
 	/**
@@ -176,13 +217,14 @@ public class Algorithm extends StricterEnchant {
 	 *            the ens
 	 * @param item
 	 *            the item
+	 * @param l
 	 * @param wild
 	 *            the wild
 	 * @param group
 	 *            the group
 	 */
-	private void f(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void f(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
@@ -190,7 +232,14 @@ public class Algorithm extends StricterEnchant {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
 		}
-		// Throw EnchantModifyEvent with the correct parameters
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
+		}
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		this.res.clear();
 	}
 
 	/**
@@ -200,20 +249,29 @@ public class Algorithm extends StricterEnchant {
 	 *            the ens
 	 * @param item
 	 *            the item
+	 * @param l
 	 * @param wild
 	 *            the wild
 	 * @param group
 	 *            the group
 	 */
-	private void g(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void g(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
 		} else if (ItemLookup.isStoneOrLeather(item)) {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
-		} // Throw EnchantModifyEvent with the correct parameters
+		}
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
+		}
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		this.res.clear();
 	}
 
 	/**
@@ -223,20 +281,29 @@ public class Algorithm extends StricterEnchant {
 	 *            the ens
 	 * @param item
 	 *            the item
+	 * @param l
 	 * @param wild
 	 *            the wild
 	 * @param group
 	 *            the group
 	 */
-	private void h(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void h(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
 		} else if (ItemLookup.isStoneOrLeather(item)) {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
-		} // Throw EnchantModifyEvent with the correct parameters
+		}
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
+		}
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		this.res.clear();
 	}
 
 	/**
@@ -246,20 +313,29 @@ public class Algorithm extends StricterEnchant {
 	 *            the ens
 	 * @param item
 	 *            the item
+	 * @param l
 	 * @param wild
 	 *            the wild
 	 * @param group
 	 *            the group
 	 */
-	private void i(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void i(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
 		} else if (ItemLookup.isStoneOrLeather(item)) {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
-		} // Throw EnchantModifyEvent with the correct parameters
+		}
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
+		}
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		this.res.clear();
 	}
 
 	/**
@@ -269,20 +345,29 @@ public class Algorithm extends StricterEnchant {
 	 *            the ens
 	 * @param item
 	 *            the item
+	 * @param l
 	 * @param wild
 	 *            the wild
 	 * @param group
 	 *            the group
 	 */
-	private void j(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void j(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
 		} else if (ItemLookup.isStoneOrLeather(item)) {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
-		} // Throw EnchantModifyEvent with the correct parameters
+		}
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
+		}
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		this.res.clear();
 	}
 
 	/**
@@ -292,20 +377,29 @@ public class Algorithm extends StricterEnchant {
 	 *            the ens
 	 * @param item
 	 *            the item
+	 * @param l
 	 * @param wild
 	 *            the wild
 	 * @param group
 	 *            the group
 	 */
-	private void k(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void k(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
 		} else if (ItemLookup.isStoneOrLeather(item)) {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
-		} // Throw EnchantModifyEvent with the correct parameters
+		}
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
+		}
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		this.res.clear();
 	}
 
 	/**
@@ -315,13 +409,14 @@ public class Algorithm extends StricterEnchant {
 	 *            the ens
 	 * @param item
 	 *            the item
+	 * @param l
 	 * @param wild
 	 *            the wild
 	 * @param group
 	 *            the group
 	 */
-	private void l(Map<Enchantment, Integer> ens, ItemStack item, Boolean wild,
-			Group group) {
+	private void l(Map<Enchantment, Integer> ens, ItemStack item, int l,
+			Boolean wild, Group group) {
 		if (ItemLookup.isDiamond(item)) {
 		} else if (ItemLookup.isIron(item)) {
 		} else if (ItemLookup.isGold(item)) {
@@ -329,47 +424,15 @@ public class Algorithm extends StricterEnchant {
 		} else if (ItemLookup.isWood(item)) {
 		} else if (ItemLookup.isChain(item)) {
 		}
-		// Throw EnchantModifyEvent with the correct parameters
-	}
-
-	/**
-	 * Notify player their enchantment was modified Tell them the difference.
-	 * 
-	 * @param player
-	 *            the player
-	 * @param original
-	 *            the original
-	 * @param result
-	 *            the result
-	 */
-	@SuppressWarnings("unused")
-	private void notify(Player player, Map<Enchantment, Integer> original,
-			Map<Enchantment, Integer> result) {
-		if (!Settings.anon) {
-			player.sendMessage(ChatColor.GREEN
-					+ "Your enchantment was modified with StrictEnchants");
-			player.sendMessage(ChatColor.YELLOW
-					+ "Enchantments are based off of the following");
-			player.sendMessage(ChatColor.GOLD + "");
+		// Create the enchantment map, ensures no wasted objects
+		if (this.res == null) {
+			this.res = new HashMap<Enchantment, Integer>();
 		}
-	}
-
-	/**
-	 * Gets the difference between the original enchant naturally produced and
-	 * the modded enchantment Produce it into a nice list for displaying to
-	 * people if Anon. enchanting is disabled
-	 * 
-	 * @param original
-	 *            the original
-	 * @param result
-	 *            the result
-	 * @return the diff
-	 */
-	@SuppressWarnings("unused")
-	private List<String> getDiff(Map<Enchantment, Integer> original,
-			Map<Enchantment, Integer> result) {
-
-		return null;
+		EnchantModifyEvent event = new EnchantModifyEvent(item, l, ens,
+				this.res);
+		Bukkit.getPluginManager().callEvent(event);
+		// Does
+		this.res.clear();
 	}
 
 	/**
